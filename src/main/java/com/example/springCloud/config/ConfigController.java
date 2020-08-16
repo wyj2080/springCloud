2ee71878341988534cb9c71f7bab0ec6f4d44b13
wangyinjia <1603068044@qq.com>
2020-08-16 23:02:07 +0800

    discovery

---
+++ src/main/java/com/example/springCloud/config/ConfigController.java
@@ -0,0 +1,26 @@
+package com.example.springCloud.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:
+ * @Author: wangyinjia
+ * @Date: 2020/8/16
+ * @Version: 1.0
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
