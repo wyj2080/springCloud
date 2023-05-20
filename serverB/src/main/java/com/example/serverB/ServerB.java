dd88fc2f8fb5d592908fb498ff2a846bdad71bc2
wyj2080 <1603068044@qq.com>
2023-05-20 19:51:44 +0800

    spring boot，nacos版本升级

--- serverB/src/main/java/com/example/serverB/ServerB.java
+++ serverB/src/main/java/com/example/serverB/ServerB.java
@@ -1,5 +1,6 @@
 package com.example.serverB;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan({"com.example.serverB.*.mapper"})
 public class ServerB {
 
 	public static void main(String[] args) {
